78c8a9eede1e313b39051830adccf5e03c2a5022
Jessawynne <jessawynne@gmail.com>
2016-01-25 16:06:27 -0600

    more tests

---
+++ tests/arrow-funcitons.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect;
+
+describe('arrow functions', function () {
+
+  it('is shorter to write', function () {
+    var fn = () => {
+      return 'Hello World';
+    };
+    expect(fn()).to.equal('Hello World');
+  });
+
+  it('one parameter can be written without parens', function () {
+    var fn = name => {
+      return `Hello ${name}`;
+    };
+    expect(fn('Jessa')).to.equal('Hello Jessa');
+  });
+
+  it('binds "this" to the function', function () {
+    this.foo = 'bar';
+    var fn = () => {
+      return this.foo;
+    };
+    expect(fn()).to.equal('bar');
+  });
+});
